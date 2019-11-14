60c07383af0e3fc455eace9d7407d0092e52e07b
HEClack <elizaclack@gmail.com>
2019-11-13 16:49:34 -0800

    added read me, cleanup compiler code 	modified:   README.md 	modified:   compiler.cpp

--- compiler.cpp
+++ compiler.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 #include <fstream>
 
-const static int BUF_SIZE = 4096;
+const static int SIZE = 4096;
 
 using std::ios_base;
 
 int main(int argc, char** argv) {
 
    std::ifstream in(argv[1],
-      ios_base::in | ios_base::binary);  // Use binary mode so we can
-   std::ofstream out(argv[2],            // handle all kinds of file
-      ios_base::out | ios_base::binary); // content.
-   
-   // Make sure the streams opened okay...
+      ios_base::in | ios_base::binary);  
+   std::ofstream out(argv[2],            
+      ios_base::out | ios_base::binary); 
 
-   char buf[BUF_SIZE];
+   char buf[SIZE];
 
    do {
-      in.read(&buf[0], BUF_SIZE);      // Read at most n bytes into
-        for (int i = 0; i<BUF_SIZE; i++){
+      in.read(&buf[0], SIZE);      
+        for (int i = 0; i<SIZE; i++){
            buf[i]=static_cast<int>(buf[i]);
         }
-      out.write(&buf[0], in.gcount()); // buf, then write the buf to
-   } while (in.gcount() > 0);          // the output.
-
-   // Check streams for problems...
-
+      out.write(&buf[0], in.gcount()); 
+   } while (in.gcount() > 0);          
    in.close();
    out.close();
 }
